08eb4d30f8edc7406c0368cee53d4800d2d82dc4
Glauber Brennon <glauberbrennon@gmail.com>
2025-01-06 21:16:42 -0300

    ValidationPipe configs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { appConfig } from '@shared/config/app.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  await app.listen(appConfig.port);
 }
+
 bootstrap();
