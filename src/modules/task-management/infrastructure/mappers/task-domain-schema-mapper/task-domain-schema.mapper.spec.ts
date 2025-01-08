6d0e8b2a014007863c321f94e813208b0e939827
Glauber Brennon <glauberbrennon@gmail.com>
2025-01-08 03:05:11 -0300

    implemented TaskDomainSchemaMapper

---
+++ src/modules/task-management/infrastructure/mappers/task-domain-schema-mapper/task-domain-schema.mapper.spec.ts
@@ -0,0 +1,27 @@
+import { TaskDomainSchemaMapper } from "./task-domain-schema.mapper";
+import { Task } from "@task-management/domain/entities/task";
+import { TaskStatus } from "@task-management/domain/value-objects/task-status";
+
+describe("TaskSchemaDomainMapper", () => {
+  const mapper = new TaskDomainSchemaMapper();
+
+  describe("map", () => {
+    const task = new Task(
+      "123",
+      "Task title",
+      "Task description",
+      new TaskStatus("IN_PROGRESS"),
+      "1"
+    );
+
+    it("should map a Task to a TaskEntity", () => {
+      const taskEntity = mapper.map(task);
+
+      expect(taskEntity.id).toBe(task.id);
+      expect(taskEntity.title).toBe(task.title);
+      expect(taskEntity.description).toBe(task.description);
+      expect(taskEntity.status).toBe(task.status);
+      expect(taskEntity.ownerId).toBe(task.ownerId);
+    });
+  });
+});
