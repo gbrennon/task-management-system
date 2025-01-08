1df5cfaf57a6c3ffbe2107c748cde3a50f5a6de2
Glauber Brennon <glauberbrennon@gmail.com>
2025-01-08 02:11:52 -0300

    impl TaskEntity

---
+++ src/modules/task-management/infrastructure/entities/task.entity.ts
@@ -0,0 +1,21 @@
+import { Entity, PrimaryColumn, Column } from 'typeorm';
+import { TaskStatus } from '../../domain/value-objects/task-status';
+import { BaseEntity } from '@shared/infrastructure/entities/BaseEntity';
+
+@Entity('tasks')
+export class TaskEntity extends BaseEntity {
+  @PrimaryColumn('uuid')
+  public id!: string;
+
+  @Column()
+  public title!: string;
+
+  @Column()
+  public description!: string;
+
+  @Column(() => TaskStatus) // Assuming TaskStatus is a value object stored as a JSON field
+  public status!: TaskStatus;
+
+  @Column()
+  public ownerId!: string;
+}
