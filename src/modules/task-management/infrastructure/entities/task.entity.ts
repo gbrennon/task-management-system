c97ee6279eaa401f8b1582edd73a2a3eea2991e6
Glauber Brennon <glauberbrennon@gmail.com>
2025-01-08 03:38:13 -0300

    fix on taskStatus field

--- src/modules/task-management/infrastructure/entities/task.entity.ts
+++ src/modules/task-management/infrastructure/entities/task.entity.ts
@@ -1,5 +1,4 @@
 import { Entity, PrimaryColumn, Column } from 'typeorm';
-import { TaskStatus } from '../../domain/value-objects/task-status';
 import { BaseEntity } from '@shared/infrastructure/entities/BaseEntity';
 
 @Entity('tasks')
@@ -13,8 +12,8 @@ export default class TaskEntity extends BaseEntity {
   @Column()
   public description!: string;
 
-  @Column(() => TaskStatus) // Assuming TaskStatus is a value object stored as a JSON field
-  public status!: TaskStatus;
+  @Column()
+  public status!: string;
 
   @Column()
   public ownerId!: string;
