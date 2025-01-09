1c2197c691d65baccfc9adba2ff403a1e1a8886c
Glauber Brennon <glauberbrennon@gmail.com>
2025-01-09 13:08:56 -0300

    app.module now depends on task-management

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,15 @@ import { AppService } from './app.service';
 import { UserManagementModule } from './modules/user-management/user-management.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppDataSource } from '@shared/infrastructure/config/app-data-source';
+import { TaskManagementModule } from './modules/task-management/task-management.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
       useFactory: async () => AppDataSource.options,
     }),
-    UserManagementModule
+    UserManagementModule,
+    TaskManagementModule
   ],
   controllers: [AppController],
   providers: [AppService],
