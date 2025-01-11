b4c3600a99bf156158f4698b3b3fbf5b3ab2ad43
Glauber Brennon <glauberbrennon@gmail.com>
2025-01-10 23:31:33 -0300

    TaskStatus now using an enum

--- src/modules/task-management/domain/value-objects/task-status.spec.ts
+++ src/modules/task-management/domain/value-objects/task-status.spec.ts
@@ -1,27 +1,41 @@
 import { InvalidTaskStatusError } from "../errors/invalid-task-status.error";
-import { TaskStatus } from "./task-status";
+import { TaskStatus, TaskStatusEnum } from "./task-status";
 
 describe('TaskStatus', () => {
   describe('fromString', () => {
     it('should return TODO when value is "TODO"', () => {
-      const taskStatus = TaskStatus.fromString('TODO');
-      expect(taskStatus).toBe(TaskStatus.TODO);
+      const stringStatus = 'TODO';
+
+      const taskStatus = TaskStatus.fromString(stringStatus);
+
+      const expectedTaskStatus = new TaskStatus(TaskStatusEnum.TODO);
+      expect(taskStatus).toStrictEqual(expectedTaskStatus);
     });
 
     it('should return IN_PROGRESS when value is "IN_PROGRESS"', () => {
-      const taskStatus = TaskStatus.fromString('IN_PROGRESS');
-      expect(taskStatus).toBe(TaskStatus.IN_PROGRESS);
+      const stringStatus = 'IN_PROGRESS';
+
+      const taskStatus = TaskStatus.fromString(stringStatus);
+
+      const expectedTaskStatus = new TaskStatus(TaskStatusEnum.IN_PROGRESS);
+      expect(taskStatus).toStrictEqual(expectedTaskStatus);
     });
 
     it('should return DONE when value is "DONE"', () => {
-      const taskStatus = TaskStatus.fromString('DONE');
-      expect(taskStatus).toBe(TaskStatus.DONE);
+      const stringStatus = 'DONE';
+
+      const taskStatus = TaskStatus.fromString(stringStatus);
+
+      const expectedTaskStatus = new TaskStatus(TaskStatusEnum.DONE);
+      expect(taskStatus).toStrictEqual(expectedTaskStatus);
     });
 
     it('should throw an error when value is invalid', () => {
+      const invalidStatus = 'INVALID';
+
       expect(() => {
-        TaskStatus.fromString('INVALID');
-      }).toThrow(new InvalidTaskStatusError('INVALID'));
+        TaskStatus.fromString(invalidStatus);
+      }).toThrow(new InvalidTaskStatusError(invalidStatus));
     });
   });
 });
--- src/modules/task-management/domain/value-objects/task-status.ts
+++ src/modules/task-management/domain/value-objects/task-status.ts
@@ -1,24 +1,21 @@
 import { InvalidTaskStatusError } from "../errors/invalid-task-status.error";
 
+export enum TaskStatusEnum {
+  TODO = 'TODO',
+  IN_PROGRESS = 'IN_PROGRESS',
+  DONE = 'DONE'
+}
+
 export class TaskStatus {
   public constructor(
     public readonly value: string
   ) {}
 
-  public static readonly TODO = new TaskStatus('TODO');
-  public static readonly IN_PROGRESS = new TaskStatus('IN_PROGRESS');
-  public static readonly DONE = new TaskStatus('DONE');
-
   public static fromString(value: string): TaskStatus {
-    switch(value) {
-      case TaskStatus.TODO.value:
-        return TaskStatus.TODO;
-      case TaskStatus.IN_PROGRESS.value:
-        return TaskStatus.IN_PROGRESS;
-      case TaskStatus.DONE.value:
-        return TaskStatus.DONE;
-      default:
-        throw new InvalidTaskStatusError(value);
+    if (!Object.values(TaskStatusEnum).includes(value as TaskStatusEnum)) {
+      throw new InvalidTaskStatusError(value);
     }
+
+    return new TaskStatus(value);
   }
 }
