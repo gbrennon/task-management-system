30767272dbce29120046bb1f6b2ff6fe763e4ad1
Glauber Brennon <glauberbrennon@gmail.com>
2025-01-11 00:12:45 -0300

    added findByIdAndOwnerId

--- src/modules/task-management/domain/ports/task.repository.ts
+++ src/modules/task-management/domain/ports/task.repository.ts
@@ -3,4 +3,5 @@ import { Task } from "../entities/task";
 export interface TaskRepository {
   save(task: Task): Promise<void>;
   findById(id: string): Promise<Task | null>;
+  findByIdAndOwnerId(id: string, ownerId: string): Promise<Task | null>;
 }
